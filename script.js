fbba094fdf6d2fb5969c3d2df18848b7650a6faa
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2021-09-29 21:11:37 +0600

    fundamental things are done

--- script.js
+++ script.js
@@ -1,11 +1,84 @@
-var userValue = document.getElementsByClassName("numb-btn");
-for (let i = 0; i < userValue.length; i++) {
-    const value = userValue[i];
-    value.addEventListener("click", function (event) {
-        var input = event.target.innerText;
-        input = +input;
-        var interface = document.getElementById("interface-display").value = input;
-        var newInterface = event.target.innerText;
-        document.getElementById("interface-display").value = interface + newInterface;
+var numSymbol = document.getElementsByClassName("num-symbol");
+for (let i = 0; i < numSymbol.length; i++) {
+    const element = numSymbol[i];
+    element.addEventListener("click", function (event) {
+        document.getElementById("interface-display").value += event.target.innerText;
     })
 }
+
+document.getElementById("clear-btn").addEventListener("click", function () {
+    document.getElementById("interface-display").value = "";
+    document.getElementById("remaining").innerText = "";
+})
+
+
+
+
+// document.getElementById("divide").addEventListener("click", function () {
+//     var remainingDisplay = document.getElementById("remaining");
+//     remainingDisplay.style.display = "block";
+//     remainingDisplay.innerText = document.getElementById("interface-display").value + "/";
+//     document.getElementById("interface-display").value = "";
+
+//     var remainingValue = remainingDisplay.innerText;
+//     // console.log("/" == remainingDisplay.innerText);
+
+// })
+
+var mathSymbol = document.getElementsByClassName("math-symbol");
+for (let i = 0; i < mathSymbol.length; i++) {
+    const element = mathSymbol[i];
+    element.addEventListener("click", function () {
+        var remainingDisplay = document.getElementById("remaining");
+        remainingDisplay.style.display = "block";
+        remainingDisplay.innerText = document.getElementById("interface-display").value + element.innerText;
+        document.getElementById("interface-display").value = "";
+
+    })
+}
+
+
+
+document.getElementById("isEqual").addEventListener("click", function () {
+    document.getElementById("result").style.display = "block";
+    var x = document.getElementById("remaining").innerText;
+    x = parseFloat(x);
+    var y = document.getElementById("interface-display").value;
+    y = parseFloat(y);
+    var xInput = document.getElementById("remaining").innerText;
+    var xDivideProperty = x + "/";
+    var xMultiplyProperty = x + "*";
+    var xMinusProperty = x + "-";
+    var xPlusProperty = x + "+";
+    console.log(xInput == xDivideProperty);
+    if (xInput == xDivideProperty) {
+        console.log(x / y);
+        document.getElementById("result").innerText =  x/ y;
+    }
+    else if (xInput == xMultiplyProperty) {
+        console.log(x * y);
+        document.getElementById("result").innerText =  x * y;
+    }
+    else if (xInput == xMinusProperty) {
+        console.log(x - y);
+        document.getElementById("result").innerText =  x - y;
+    }
+    else if (xInput == xPlusProperty) {
+        console.log(x + y);
+        document.getElementById("result").innerText =  x + y;
+    }
+
+
+
+
+
+    // if (symbol == "/" && x + symbol === xInput) {
+    //     console.log(x / y);
+    // }
+    // else if (symbol == "*" && x + symbol === xInput) {
+    //     console.log(x * y);
+    // }
+    // console.log(x+symbol === xInput);
+    // console.log(x);
+
+})
